fb6cceaf8d7ea7809596d6f6313039d680376b4a
Phelim <phelim.brady@moypark.com>
2017-04-28 11:44:11 +0100

    Add project files.

---
+++ Doodle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doodle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new WPAnalyticsEntities())
+            {
+                context.Database.Log = Console.Write;
+
+                var test = (from p in context.SubDivisons
+                            select new { ID = p.ID, Name = p.SubDivisionName }).ToList();
+                context.SaveChangesAsync().Wait();
+                Console.ReadLine()
+            }
+
+        }
+    }
+}
