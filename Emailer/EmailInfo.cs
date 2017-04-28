8888e12bb45259bd4a3d8f135986c2125f019455
Phelim <phelim.brady@moypark.com>
2017-04-28 12:12:34 +0100

    interface added and emailinfo object created

---
+++ Emailer/EmailInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emailer
+{
+ public   class EmailInfo
+    {
+        public string Body { get; set; }
+        public string Subject { get; set; }
+        public string EmailTo { get; set; }
+        public string EmailFrom { get; set; }
+        public List<String> AttachmentFilePaths { get; set; }
+    }
+}
