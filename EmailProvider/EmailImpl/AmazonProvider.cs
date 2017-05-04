043784ce761cbb919042b3ede62d30f9bebd24ca
Paul McC <paulmccabe51@hotmail.com>
2017-05-04 19:20:39 +0100

    Moved Email bits out to separate party project

---
+++ EmailProvider/EmailImpl/AmazonProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmailProvider.EmailImpl
+{
+    public class AmazonProvider : IEmailService
+    {
+        public bool sendMail(EmailInfo Obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EmailProvider/EmailInfo.cs
+++ EmailProvider/EmailInfo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Emailer
+namespace EmailProvider
 {
  public   class EmailInfo
     {
